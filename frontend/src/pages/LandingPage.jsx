b6380964caa55736dd42df3b54d1835e8b17d6f3
DharanSJIT <dharan.mj2k@gmail.com>
2026-01-04 16:41:04 +0530

    updated ui

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -48,7 +48,7 @@ const LandingPage = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50" style={{backgroundImage: 'url(/landing_bg.png)', backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}>
       {/* Header */}
       <header className="fixed top-0 left-0 right-0 z-50 bg-white border-b border-gray-200 shadow-sm">
         <div className="max-w-[85vw] mx-auto px-4 sm:px-6 lg:px-8">
